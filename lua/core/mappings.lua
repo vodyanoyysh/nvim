3fba2a8cf5d38595c1e2394f38a6fd9a43d3e836
d.a.kolesov <d.a.kolesov@s7.ru>
2025-06-18 09:03:09 +0700

    commit

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -7,7 +7,7 @@ vim.keymap.set("n", "<leader>e", ":Neotree float toggle<CR>")
 vim.keymap.set("n", "<leader>gg", ":Neotree float git_status toggle<CR>")
 
 -- BufferLine
-vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>") -- next tab
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")     -- next tab
 vim.keymap.set("n", "<s-Tab>", ":BufferLineCyclePrev<CR>")
 vim.keymap.set("n", "<leader>c", ":BufferLinePickClose<CR>") -- close tab
 
@@ -40,7 +40,7 @@ vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<leader>w", ":w<CR>") -- save
 
 vim.keymap.set("n", "<leader>fc", function()
-	require("conform").format({ bufnr = 0 })
+    require("conform").format({ bufnr = 0 })
 end, { desc = "Форматировать текущий буфер" })
 
 -- database
@@ -54,52 +54,52 @@ vim.keymap.set("n", "<leader>dvo", ":DiffviewOpen<CR>", { desc = "Diffview open"
 vim.keymap.set("n", "<leader>dvc", ":DiffviewClose<CR>", { desc = "Diffview close" })
 
 vim.keymap.set('n', '<leader>r', function()
-  local resize_mode = true
-  local status_msg = "РЕЖИМ ИЗМЕНЕНИЯ РАЗМЕРА ОКОН | h/l - ширина | j/k - высота | q/<Esc> - выход"
-  
-  -- Показываем сообщение в статусной строке
-  vim.api.nvim_echo({{status_msg, "WarningMsg"}}, false, {})
-  
-  -- Создаем локальные маппинги для этого буфера
-  local opts = {buffer = 0, silent = true}
-  
-  -- Маппинги для изменения размеров
-  vim.keymap.set('n', 'h', ':vertical resize -5<CR>', opts)
-  vim.keymap.set('n', 'l', ':vertical resize +5<CR>', opts)
-  vim.keymap.set('n', 'j', ':resize +5<CR>', opts)
-  vim.keymap.set('n', 'k', ':resize -5<CR>', opts)
-  
-  -- Маппинги для более точной настройки
-  vim.keymap.set('n', 'H', ':vertical resize -1<CR>', opts)
-  vim.keymap.set('n', 'L', ':vertical resize +1<CR>', opts)
-  vim.keymap.set('n', 'J', ':resize +1<CR>', opts)
-  vim.keymap.set('n', 'K', ':resize -1<CR>', opts)
-  
-  -- Выход из режима
-  local exit_func = function()
-    vim.keymap.del('n', 'h', {buffer = 0})
-    vim.keymap.del('n', 'l', {buffer = 0})
-    vim.keymap.del('n', 'j', {buffer = 0})
-    vim.keymap.del('n', 'k', {buffer = 0})
-    vim.keymap.del('n', 'H', {buffer = 0})
-    vim.keymap.del('n', 'L', {buffer = 0})
-    vim.keymap.del('n', 'J', {buffer = 0})
-    vim.keymap.del('n', 'K', {buffer = 0})
-    vim.keymap.del('n', '<Esc>', {buffer = 0})
-    vim.keymap.del('n', 'q', {buffer = 0})
-    vim.api.nvim_echo({{"Режим изменения размера выключен", "Normal"}}, false, {})
-  end
-  
-  vim.keymap.set('n', '<Esc>', exit_func, opts)
-  vim.keymap.set('n', 'q', exit_func, opts)
-end, {desc = 'Войти в режим изменения размеров окон'})
+    local resize_mode = true
+    local status_msg = "РЕЖИМ ИЗМЕНЕНИЯ РАЗМЕРА ОКОН | h/l - ширина | j/k - высота | q/<Esc> - выход"
+
+    -- Показываем сообщение в статусной строке
+    vim.api.nvim_echo({ { status_msg, "WarningMsg" } }, false, {})
+
+    -- Создаем локальные маппинги для этого буфера
+    local opts = { buffer = 0, silent = true }
+
+    -- Маппинги для изменения размеров
+    vim.keymap.set('n', 'h', ':vertical resize -5<CR>', opts)
+    vim.keymap.set('n', 'l', ':vertical resize +5<CR>', opts)
+    vim.keymap.set('n', 'j', ':resize +5<CR>', opts)
+    vim.keymap.set('n', 'k', ':resize -5<CR>', opts)
+
+    -- Маппинги для более точной настройки
+    vim.keymap.set('n', 'H', ':vertical resize -1<CR>', opts)
+    vim.keymap.set('n', 'L', ':vertical resize +1<CR>', opts)
+    vim.keymap.set('n', 'J', ':resize +1<CR>', opts)
+    vim.keymap.set('n', 'K', ':resize -1<CR>', opts)
+
+    -- Выход из режима
+    local exit_func = function()
+        vim.keymap.del('n', 'h', { buffer = 0 })
+        vim.keymap.del('n', 'l', { buffer = 0 })
+        vim.keymap.del('n', 'j', { buffer = 0 })
+        vim.keymap.del('n', 'k', { buffer = 0 })
+        vim.keymap.del('n', 'H', { buffer = 0 })
+        vim.keymap.del('n', 'L', { buffer = 0 })
+        vim.keymap.del('n', 'J', { buffer = 0 })
+        vim.keymap.del('n', 'K', { buffer = 0 })
+        vim.keymap.del('n', '<Esc>', { buffer = 0 })
+        vim.keymap.del('n', 'q', { buffer = 0 })
+        vim.api.nvim_echo({ { "Режим изменения размера выключен", "Normal" } }, false, {})
+    end
+
+    vim.keymap.set('n', '<Esc>', exit_func, opts)
+    vim.keymap.set('n', 'q', exit_func, opts)
+end, { desc = 'Войти в режим изменения размеров окон' })
 
 
 local diagnostic_enabled = true -- Начальное состояние (включено)
 
 vim.keymap.set('n', '<leader>le', function()
     diagnostic_enabled = not diagnostic_enabled
-    
+
     if diagnostic_enabled then
         -- Включаем отображение диагностики в строке
         vim.diagnostic.config({
@@ -127,12 +127,12 @@ end, { desc = 'Включение/выключение диагностики о
 vim.keymap.set('n', '<leader>lc', function()
     local line = vim.fn.line('.')
     local diagnostics = vim.diagnostic.get(0, { lnum = line - 1 })
-    
+
     if #diagnostics == 0 then
         vim.notify("На этой строке нет диагностических сообщений", vim.log.levels.WARN)
         return
     end
-    
+
     -- Создаем текст из всех диагностических сообщений на строке
     local messages = {}
     for _, diagnostic in ipairs(diagnostics) do
@@ -148,9 +148,9 @@ vim.keymap.set('n', '<leader>lc', function()
         end
         table.insert(messages, prefix .. diagnostic.message)
     end
-    
+
     local full_message = table.concat(messages, "\n")
-    vim.fn.setreg('+', full_message)  -- Копируем в системный буфер обмена (+ регистр)
+    vim.fn.setreg('+', full_message) -- Копируем в системный буфер обмена (+ регистр)
     vim.notify("Диагностическое сообщение скопировано:\n" .. full_message, vim.log.levels.INFO)
 end, { desc = 'Копировать текст ошибки в строке' })
 

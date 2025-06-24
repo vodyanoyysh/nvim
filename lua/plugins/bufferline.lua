f07ec878d76f5bdfbcd278fe24c3742f47262bab
d.a.kolesov <d.a.kolesov@s7.ru>
2025-06-24 08:46:21 +0700

    commit

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -12,9 +12,23 @@ require("bufferline").setup {
         },
         diagnostics = "nvim_lsp",
         indicator = {
-            icon = '  ', -- this should be omitted if indicator style is not 'icon'
+            icon = '  ', -- this should be omitted if indicator style is not 'icon'
             style = 'icon'
         },
         -- separator_style = "slope"
     }
 }
+
+-- Функция для закрытия всех буферов
+local function close_all_buffers()
+    local buffers = vim.api.nvim_list_bufs()
+    for _, buffer in ipairs(buffers) do
+        if vim.api.nvim_buf_is_valid(buffer) and vim.api.nvim_buf_get_option(buffer, 'buflisted') then
+            vim.api.nvim_buf_delete(buffer, { force = false })
+        end
+    end
+end
+
+-- Маппинг на leader + ca для закрытия всех буферов
+vim.keymap.set('n', '<leader>ca', close_all_buffers, { noremap = true, silent = true, desc = "Закрыть все буферы" })
+
